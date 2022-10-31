213cff9418514003584fe6e9d4dacc50f9918c14
MaxDePro <xmaksg@gmail.com>
2022-10-31 16:37:58 +0200

    changes

--- user_data/serializers.py
+++ user_data/serializers.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import re
 
 # xml_file = "/Users/user/PycharmProjects/test_task_django/test_task/test_task.xml"
 
@@ -13,9 +14,9 @@ def parse_xml_data(xml_file):
         try:
             first_names = us.find('first_name').text
             last_names = us.find('last_name').text
-            if first_names is not None and last_names is not None:
-                outer_dict['first_name'] = first_names
-                outer_dict['last_name'] = last_names
+            if first_names is not None and last_names is not None and not first_names.startswith('('):
+                outer_dict['first_name'] = re.sub('\(.*?\)$|\(.*?\)|\[.*?\]$|\[.*?\]', '', first_names)
+                outer_dict['last_name'] = re.sub('\(.*?\)$|\(.*?\)|\[.*?\]$|\[.*?\]', '', last_names)
                 final_data_dict[us.attrib.get('id')] = outer_dict
         except AttributeError:
             continue
